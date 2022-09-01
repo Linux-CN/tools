2495e46380ffb9eafdaa483b27d10ea4e1b72cb4
白宦成 <bestony@linux.com>
2022-09-02 00:06:41 +0800

    feat: add shiki for code highlight

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,7 @@
     guid: {{$post->guid}}<br>
     keywords:{{$post->keywords}}<br>
     excerpt: {{$post->excerpt}}<br>
-    content:<x-markdown>{{$post->content}}</x-markdown>
+    content:<x-markdown theme="slack-ochin">{{$post->content}}</x-markdown>
 <br>
     image:<img src="{{$post->thumbnail}}"/><br>
     createdAt: {{$post->created_at}}
