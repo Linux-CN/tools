34e8af87e65ef303bbc9d5f590a49907293d5543
白宦成 <bestony@linux.com>
2022-09-03 21:27:57 +0800

    feat: add auth layout & fix login erro page layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Linux 中国') }}</title>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <!-- Styles -->
+    @livewireStyles
+</head>
+
+<body>
+    <div>
+        {{ $slot }}
+    </div>
+
+</body>
+
+</html>
