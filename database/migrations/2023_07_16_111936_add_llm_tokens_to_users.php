1a167b11d18a43c5dd6d5e080e60b7301a817a6c
白宦成 <bestony@linux.com>
2023-07-16 11:20:54 +0800

    feat: add llm tokens into users

---
+++ database/migrations/2023_07_16_111936_add_llm_tokens_to_users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('llm_tokens')->default(0)->comment("user's llm tokens, can be consume in completions mod");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->removeColumn('llm_tokens');
+        });
+    }
+};
