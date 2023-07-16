2e271ac3db7a36692b500833e63e10e23dbfb750
白宦成 <bestony@linux.com>
2023-07-16 10:18:19 +0800

    feat: add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo -u www git pull
+sudo -u www yarn install --production
+sudo -u www yarn run build
+sudo -u www /usr/local/php/bin/php artisan route:clear
+sudo -u www /usr/local/php/bin/php artisan config:clear
+sudo -u www /usr/local/php/bin/php artisan view:clear
+sudo -u www /usr/local/php/bin/php /usr/local/bin/composer install  --prefer-dist --no-dev -o
+sudo -u www /usr/local/php/bin/php artisan migrate
+sudo -u www /usr/local/php/bin/php artisan route:cache
+sudo -u www /usr/local/php/bin/php artisan config:cache
+sudo -u www /usr/local/php/bin/php artisan view:cache
+sudo -u www /usr/local/php/bin/php artisan lang:update
