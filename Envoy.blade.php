7bfcc338a51855991d6d7ed38ba7b732ae8defc1
bestony <bestony@linux.com>
2023-07-23 01:27:39 +0800

    feat: reorg envoy and add local task

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,9 @@
-@servers(['web' => 'do'])
+@servers(['server' => 'do','local' =>'127.0.0.1'])
+
+{{-- story for run, only run story for server --}}
 
 @story('deploy')
+push
 update-code
 install-dependencies
 migrate
@@ -8,17 +11,21 @@ rebuild-cache
 @endstory
 
 @story('update')
+push
 update-code
 @endstory('update')
 
 
-@task('update-code')
+{{-- command for server --}}
+
+
+@task('update-code',[ 'on' => 'server'])
 cd /data/wwwroot/alpha.linux.cn
 git pull
 chown -R www.www /data/wwwroot/alpha.linux.cn
 @endtask
 
-@task('install-dependencies')
+@task('install-dependencies',[ 'on' => 'server'])
 cd /data/wwwroot/alpha.linux.cn
 sudo -u www /usr/local/php/bin/php /usr/local/bin/composer install  --prefer-dist --no-dev -o
 npm install
@@ -28,7 +35,7 @@ chown -R www.www /data/wwwroot/alpha.linux.cn/vendor
 @endtask
 
 
-@task('rebuild-cache')
+@task('rebuild-cache',[ 'on' => 'server'])
 cd /data/wwwroot/alpha.linux.cn
 sudo -u www /usr/local/php/bin/php artisan route:clear
 sudo -u www /usr/local/php/bin/php artisan config:clear
@@ -38,7 +45,14 @@ sudo -u www /usr/local/php/bin/php artisan config:cache
 sudo -u www /usr/local/php/bin/php artisan view:cache
 @endtask
 
-@task('migrate')
+
+@task('migrate',[ 'on' => 'server'])
 cd /data/wwwroot/alpha.linux.cn
 sudo -u www /usr/local/php/bin/php artisan migrate --force
+@endtask
+
+{{-- command for local --}}
+
+@task('push',['on' => 'local' ])
+git push
 @endtask
