a844d76c500aa86d93a7ad45487892ef9919cdf6
bestony <bestony@linux.com>
2023-07-23 01:19:00 +0800

    feat: update envoy command

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,29 +1,43 @@
 @servers(['web' => 'do'])
 
-@setup
-$now = new DateTime;
-@endsetup
+@story('deploy')
+update-code
+install-dependencies
+rebuild-cache
+@endstory
 
-@task('deploy')
+@story('update')
+update-code
+@endstory('update')
+
+
+@task('update-code')
     cd /data/wwwroot/alpha.linux.cn
-    git pull 
+    git pull
     chown -R www.www /data/wwwroot/alpha.linux.cn
 @endtask
 
-@task('full-deploy')
-
+@task('install-dependencies')
 cd /data/wwwroot/alpha.linux.cn
-git pull origin master
-chown -R www.www /data/wwwroot/alpha.linux.cn
-sudo -u www yarn install
-sudo -u www yarn run build
+sudo -u www /usr/local/php/bin/php /usr/local/bin/composer install  --prefer-dist --no-dev -o
+npm install
+npm run build
+chown -R www.www /data/wwwroot/alpha.linux.cn/public
+chown -R www.www /data/wwwroot/alpha.linux.cn/vendor
+@endtask
+
+
+@task('rebuild-cache')
+cd /data/wwwroot/alpha.linux.cn
 sudo -u www /usr/local/php/bin/php artisan route:clear
 sudo -u www /usr/local/php/bin/php artisan config:clear
 sudo -u www /usr/local/php/bin/php artisan view:clear
-sudo -u www /usr/local/php/bin/php /usr/local/bin/composer install  --prefer-dist --no-dev -o
-sudo -u www /usr/local/php/bin/php artisan migrate --force
 sudo -u www /usr/local/php/bin/php artisan route:cache
 sudo -u www /usr/local/php/bin/php artisan config:cache
 sudo -u www /usr/local/php/bin/php artisan view:cache
-sudo -u www /usr/local/php/bin/php artisan lang:update
+@endtask
+
+@task('migrate')
+cd /data/wwwroot/alpha.linux.cn
+    sudo -u www /usr/local/php/bin/php artisan migrate --force
 @endtask
