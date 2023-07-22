420980124c55785981acfac36232ba116a9c775f
bestony <bestony@linux.com>
2023-07-23 00:50:53 +0800

    feat: add code

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
 chown -R www.www /data/wwwroot/alpha.linux.cn
-find /data/wwwroot/alpha.linux.cn -type d -exec chmod 755 {} \;
-find /data/wwwroot/alpha.linux.cn -type f -exec chmod 644 {} \;
+#find /data/wwwroot/alpha.linux.cn -type d -exec chmod 755 {} \;
+#find /data/wwwroot/alpha.linux.cn -type f -exec chmod 644 {} \;
 sudo -u www yarn install
 sudo -u www yarn run build
 sudo -u www /usr/local/php/bin/php artisan route:clear
