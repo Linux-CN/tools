2f5b3f966e62f2552f131811a8b4acc4f609d257
bestony <bestony@linux.com>
2023-07-23 03:29:37 +0800

    feat: add current token feature

--- resources/views/livewire/faucet.blade.php
+++ resources/views/livewire/faucet.blade.php
@@ -9,7 +9,7 @@
         @else
     <button type="button"
             class="rounded-md bg-gray-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
-        {{__('You have claim today\'s token')}}
+        {{__('You have already claim today token. Try again tomorrow.')}}
     </button>
     @endif
 </div>
