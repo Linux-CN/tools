14c8333d4f7032dfced34745bd78171b7e762dcd
bestony <bestony@linux.com>
2023-07-23 06:15:41 +0800

    feat: replace logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,30 @@
 <a href="/">
-    <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
-    </svg>
+    <svg class="w-16 h-16" height="100%" stroke-miterlimit="10" style="fill-rule:nonzero;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;" version="1.1" viewBox="99.214 80.5 343 343" width="100%" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:vectornator="http://vectornator.io" xmlns:xlink="http://www.w3.org/1999/xlink">
+<defs/>
+        <clipPath id="ArtboardFrame">
+            <rect height="343" width="343" x="99.214" y="80.5"/>
+        </clipPath>
+        <g clip-path="url(#ArtboardFrame)" id="Layer" vectornator:layerName="Layer">
+            <g opacity="1" vectornator:layerName="图片">
+                <path d="M171.714 153L99.714 153L99.714 81L171.714 81L171.714 153Z" fill="#00a8e6" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M171.714 243L99.714 243L99.714 171L171.714 171L171.714 243Z" fill="#00a8e6" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M171.714 333L99.714 333L99.714 261L171.714 261L171.714 333Z" fill="#00a8e6" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M261.714 423L189.714 423L189.714 351L261.714 351L261.714 423Z" fill="#2c292a" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M261.714 333L189.714 333L189.714 261L261.714 261L261.714 333Z" fill="#00a8e6" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M441.714 423L369.714 423L369.714 351L441.714 351L441.714 423Z" fill="#26b34f" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M351.714 333L279.714 333L279.714 261L351.714 261L351.714 333Z" fill="#26b34f" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M441.714 333L369.714 333L369.714 261L441.714 261L441.714 333Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+                <path d="M261.714 243L189.714 243L189.714 171L261.714 171L261.714 243Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+                <path d="M351.714 243L279.714 243L279.714 171L351.714 171L351.714 243Z" fill="#26b34f" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M441.714 243L369.714 243L369.714 171L441.714 171L441.714 243Z" fill="#26b34f" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M261.714 153L189.714 153L189.714 81L261.714 81L261.714 153Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+                <path d="M351.714 153L279.714 153L279.714 81L351.714 81L351.714 153Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+                <path d="M441.714 153L369.714 153L369.714 81L441.714 81L441.714 153Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+                <path d="M351.714 423L279.714 423L279.714 351L351.714 351L351.714 423Z" fill="#26b34f" fill-rule="nonzero" opacity="1" stroke="none" vectornator:layerName="path"/>
+                <path d="M171.714 423L99.714 423L99.714 351L171.714 351L171.714 423Z" fill="none" opacity="1" stroke="#959799" stroke-linecap="butt" stroke-linejoin="miter" stroke-width="1" vectornator:layerName="path"/>
+            </g>
+        </g>
+</svg>
 </a>
+
+
